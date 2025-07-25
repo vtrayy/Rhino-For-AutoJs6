4a3f928264b8f5ad95947f1597b276c58a63201a
Duncan MacGregor <duncan.macgregor@servicenow.com>
2025-07-25 02:43:03 +0100

    Interpreter boxing / unboxing reduction
    Optimize some math operations to operate on primitive numbers rather than always boxing all numbers before operating on them.
    
    (cherry picked from commit 832c1088b79308595027e6e368020507ab66d913)

--- rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
+++ rhino/src/main/java/org/mozilla/javascript/ScriptRuntime.java
@@ -3801,6 +3801,10 @@ public class ScriptRuntime {
         }
     }
 
+    public static double bitwiseAND(double val1, double val2) {
+        return (double) (toInt32(val1) & toInt32(val2));
+    }
+
     public static Number bitwiseAND(Number val1, Number val2) {
         if (val1 instanceof BigInteger && val2 instanceof BigInteger) {
             return ((BigInteger) val1).and((BigInteger) val2);
@@ -3812,6 +3816,10 @@ public class ScriptRuntime {
             int result = toInt32(val1.doubleValue()) & toInt32(val2.doubleValue());
             return Double.valueOf(result);
         }
+    }
+
+    public static double bitwiseOR(double val1, double val2) {
+        return (double) (toInt32(val1) | toInt32(val2));
     }
 
     public static Number bitwiseOR(Number val1, Number val2) {
@@ -3827,6 +3835,10 @@ public class ScriptRuntime {
         }
     }
 
+    public static double bitwiseXOR(double val1, double val2) {
+        return (double) (toInt32(val1) ^ toInt32(val2));
+    }
+
     public static Number bitwiseXOR(Number val1, Number val2) {
         if (val1 instanceof BigInteger && val2 instanceof BigInteger) {
             return ((BigInteger) val1).xor((BigInteger) val2);
@@ -3838,6 +3850,10 @@ public class ScriptRuntime {
             int result = toInt32(val1.doubleValue()) ^ toInt32(val2.doubleValue());
             return Double.valueOf(result);
         }
+    }
+
+    public static double leftShift(double val1, double val2) {
+        return (double) (toInt32(val1) << toInt32(val2));
     }
 
     @SuppressWarnings("AndroidJdkLibsChecker")
@@ -3858,6 +3874,10 @@ public class ScriptRuntime {
             int result = toInt32(val1.doubleValue()) << toInt32(val2.doubleValue());
             return Double.valueOf(result);
         }
+    }
+
+    public static double signedRightShift(double val1, double val2) {
+        return (double) (toInt32(val1) >> toInt32(val2));
     }
 
     @SuppressWarnings("AndroidJdkLibsChecker")
@@ -4686,7 +4706,7 @@ public class ScriptRuntime {
         }
     }
 
-    private static boolean compareTo(double d1, double d2, int op) {
+    static boolean compareTo(double d1, double d2, int op) {
         switch (op) {
             case Token.GE:
                 return d1 >= d2;
